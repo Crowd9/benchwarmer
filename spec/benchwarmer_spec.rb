81e9a66cd2bf369ab3eef57729277542dd36a467
SamuelMoreira <genesis.samuel@gmail.com>
2026-04-09 11:36:18 +0100

    Modernize gem

--- spec/benchwarmer_spec.rb
+++ spec/benchwarmer_spec.rb
@@ -4,34 +4,35 @@ module Benchwarmer
   describe API do
 
     it "requires an API token on initialization" do
-      expect {Benchwarmer::API.new}.should raise_error(ArgumentError)
+      expect {Benchwarmer::API.new}.to raise_error(ArgumentError)
     end
 
     it "also accepts config options on initialization" do
-      expect {Benchwarmer::API.new('api_token', {:secure => true})}.should_not raise_error(ArgumentError)
+      expect {Benchwarmer::API.new('api_token', {:secure => true})}.not_to raise_error
     end
 
     it "requires a username and password for class methods" do
-      expect {Benchwarmer::API.login('password')}.should raise_error(ArgumentError)
-      expect {Benchwarmer::API.token_get('password')}.should raise_error(ArgumentError)
-      expect {Benchwarmer::API.token_add('password')}.should raise_error(ArgumentError)
-      expect {Benchwarmer::API.token_delete('password')}.should raise_error(ArgumentError)
+      expect {Benchwarmer::API.login('password')}.to raise_error(ArgumentError)
+      expect {Benchwarmer::API.token_get('password')}.to raise_error(ArgumentError)
+      expect {Benchwarmer::API.token_add('password')}.to raise_error(ArgumentError)
+      expect {Benchwarmer::API.token_delete('password')}.to raise_error(ArgumentError)
     end
 
     it "requires a token to delete" do
-      expect {Benchwarmer::API.token_delete('username', 'password')}.should raise_error(ArgumentError)
+      expect {Benchwarmer::API.token_delete('username', 'password')}.to raise_error(ArgumentError)
     end
 
     it "also accepts config options on class methods" do
-      expect {Benchwarmer::API.login('username', 'password', {:secure => true})}.should_not raise_error(ArgumentError)
-      expect {Benchwarmer::API.token_get('username', 'password', {:secure => true})}.should_not raise_error(ArgumentError)
-      expect {Benchwarmer::API.token_add('username', 'password', {:secure => true})}.should_not raise_error(ArgumentError)
-      expect {Benchwarmer::API.token_delete('username', 'password', 'token', {:secure => true})}.should_not raise_error(ArgumentError)
+      allow_any_instance_of(XMLRPC::Client).to receive(:call).and_return(nil)
+      expect {Benchwarmer::API.login('username', 'password', {:secure => true})}.not_to raise_error
+      expect {Benchwarmer::API.token_get('username', 'password', {:secure => true})}.not_to raise_error
+      expect {Benchwarmer::API.token_add('username', 'password', {:secure => true})}.not_to raise_error
+      expect {Benchwarmer::API.token_delete('username', 'password', 'token', {:secure => true})}.not_to raise_error
     end
 
     describe 'Accept-Encoding header' do
       shared_examples_for 'set Accept-Encoding: identity' do
-        it { subject.http_header_extra["accept-encoding"].should eq "identity" }
+        it { expect(subject.http_header_extra["accept-encoding"]).to eq "identity" }
       end
 
       it_behaves_like 'set Accept-Encoding: identity' do
